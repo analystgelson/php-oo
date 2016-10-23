75c21a27cc609bec010a9880145dc22735c52caa
Gelson Moraes <analyst.gelson@gmail.com>
2016-10-23 19:18:22 -0300

    Trait

--- banco/ContaAbstract.php
+++ banco/ContaAbstract.php
@@ -2,23 +2,39 @@
 
 abstract class ContaAbstract
 {
-
+	use ProcessoTrait;
+	
 	protected $saldo;
+
+	public function iniciaProcesso()
+	{
+		echo "----- Inciando processo -----";
+	}
+
+	public function finalizaProcesso()
+	{
+		echo "----- Finaliza processo -----";
+	}
 
 	//o "final" não deixa o método ser sobrescrevido por class filha
 	public final function depositar($valor)
 	{
+		$this->iniciaProcesso();
 		$this->saldo += $this->calculoDeposito($valor);
+		$this->finalizaProcesso();
 		return true;
 
 	}
 
 	public function sacar($valor)
 	{
+		$this->iniciaProcesso();
 		if ( $this->saldo >= $valor ) {
 			$this->saldo -= $valor;
+			$this->finalizaProcesso();
 			return true;
 		}
+		$this->finalizaProcesso();
 		return false;
 	}
 
---
+++ banco/ProcessoTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+trait ProcessoTrait
+{
+	public function iniciaProcesso()
+	{
+		echo "----- Inciando processo -----";
+	}
+
+	public function finalizaProcesso()
+	{
+		echo "----- Finaliza processo -----";
+	}
+}
